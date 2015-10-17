19fc9e90c6b808ac8b4550edc5a094f6161bca03
Matthew Eisler (mge987) <mge987@mail.usask.ca>
2015-10-17 16:02:03 -0600

    Added struct to defs.h

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -8,6 +8,7 @@ struct rtcdate;
 struct spinlock;
 struct stat;
 struct superblock;
+struct queue;
 
 // bio.c
 void            binit(void);
@@ -119,7 +120,7 @@ int             wait(void);
 int		waitstat(int *, int*);
 void            wakeup(void*);
 void            yield(void);
-void		queuePush(struct queue, struct proc);
+void		queuePush(struct queue*, struct proc*);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
